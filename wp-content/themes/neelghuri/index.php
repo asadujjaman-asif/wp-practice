49534dc369a80eaa9e7be7f70c13fae5039641b9
asadujjaman-asif <mdasadurjjaman@gmail.com>
2023-09-11 23:12:07 +0600

    task:added new function

--- wp-content/themes/neelghuri/index.php
+++ wp-content/themes/neelghuri/index.php
@@ -9,9 +9,9 @@
 <head>
     <meta charset="<?php bloginfo('charset')?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <?php wp_head(); ?>
 </head>
-<body>
-    
+<body <?php body_class();?>>
+<?php wp_footer();?>
 </body>
 </html>
