0efecddb6a5ba472902255a570a98dea5bc0d1e1
asadujjaman-asif <mdasadurjjaman@gmail.com>
2023-09-14 16:41:16 +0600

    task:added some code

--- wp-content/themes/neelghuri/functions.php
+++ wp-content/themes/neelghuri/functions.php
@@ -7,14 +7,14 @@ add_theme_support('title_tag');
 // Load css and javascript
 function loadCssAndJs() {
     wp_enqueue_style('ng-style',get_stylesheet_uri());
-    wp_register_style('bootstrap',get_stylesheet_uri('/css/bootstrap.min.css'),array(),'5.2.3','all');
+    wp_register_style('bootstrap', get_template_directory_uri().'/css/bootstrap.min.css', array(),'5.2.3','all');
     wp_enqueue_style('bootstrap');
-    wp_register_style('custom',get_stylesheet_uri('/css/custom.min.css'),array(),'1.0.0','all');
+    wp_register_style('custom',get_template_directory_uri().'/css/custom.min.css,array()','1.0.0','all');
     wp_enqueue_style('custom');
     
     //Jquery
     wp_enqueue_script('jquery');
-    wp_enqueue_script('bootstrap',get_stylesheet_uri('/js/bootstrap.min.js'),array(),'5.3.1','true');
-    wp_enqueue_script('main',get_stylesheet_uri('/js/main.js'),array(),'1.0.0','true');
+    wp_enqueue_script('bootstrap',get_template_directory_uri().'/js/bootstrap.min.js', array(),'5.3.1','true');
+    wp_enqueue_script('main',get_template_directory_uri().'/js/main.js', array(),'1.0.0','true');
 }
 add_action('wp_enqueue_scripts','loadCssAndJs');
--- wp-content/themes/neelghuri/index.php
+++ wp-content/themes/neelghuri/index.php
@@ -5,13 +5,25 @@
 */
 ?>
 <!DOCTYPE html>
-<html lang="<?php language_attributes()?>" class="no-js">
+<html lang="<?php language_attributes(); ?>" class="no-js">
 <head>
     <meta charset="<?php bloginfo('charset')?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class();?>>
+<div id="header_area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+asas
+            </div>
+            <div class="col-md-9">
+aasa
+            </div>
+        </div>
+    </div>
+</div>
 <?php wp_footer();?>
 </body>
 </html>
